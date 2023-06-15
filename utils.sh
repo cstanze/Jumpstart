6d6fdcf3c3634d39db9755e15c8f77c8b7b76bf3
Jules Nieves <jules.nieves0301@gmail.com>
2023-06-15 14:53:48 -0400

    debug again

--- utils.sh
+++ utils.sh
@@ -140,6 +140,8 @@ dialog_friendly_timezones() {
     timezone_list+=("$timezone" "$index")
     index=$((index + 1))
   done
+
+  echo "${timezone_list[@]}"
 }
 
 dialog_friendly_locales() {
@@ -155,18 +157,22 @@ dialog_friendly_locales() {
     locale_list+=("$locale" "$index")
     index=$((index + 1))
   done
+
+  echo "${locale_list[@]}"
 }
 
 dialog_friendly_disks() {
   # `dialog --menu` expects a list of options like so:
   #   <tag> <item>
   # where `tag` is a unique identifier, `item` is the text to display
-  
-  local disks=($(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | sort -k 1))
+
+  local disks=($(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | sort -k 1 | sed 's/ /./g'))
   local disks_list=()
   local index=1
   for disk in $disks; do
     disks_list+=("$disk" "$index")
     index=$((index + 1))
   done
+
+  echo "${disks_list[@]}"
 }
